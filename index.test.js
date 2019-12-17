21e29bb7a2ce77ffd2fdedae354d7832e3bda157
Ngo Minh <nvminh023@gmail.com>
2019-12-17 17:24:31 +0700

    init

--- index.test.js
+++ index.test.js
@@ -2,10 +2,3 @@ test('Should return plus', () => {
   expect(2 + 2).toBe(4);
 });
 
-test('Should return plus', () => {
-  expect(2 + 1).toBe(4);
-});
-
-test('Should return plus', () => {
-  expect(1 + 1).toBe(4);
-});
