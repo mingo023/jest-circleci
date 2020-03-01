f9fa69180097852ac3b76f98c15e755a5af0dffa
Ngo Minh <nvminh023@gmail.com>
2020-03-01 10:13:11 +0700

    Change add port env

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = process.env.NODE_ENV || 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.status(200).json({ msg: 'Hello World!' }));
 
