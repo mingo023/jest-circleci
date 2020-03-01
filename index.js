fc95e21c0937584bdac749ed105f2b708c5e51df
Ngo Minh <nvminh023@gmail.com>
2020-03-01 10:02:28 +0700

    Add NODE_ENV

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.NODE_ENV || 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
