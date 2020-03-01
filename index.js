c681168a1f0738e87efb5d09da67dd213e8f23c0
Ngo Minh <nvminh023@gmail.com>
2020-03-01 09:54:27 +0700

    Remove mongoose

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
 
 const app = express();
 const port = 3000;
-
-mongoose
-  .connect('mongodb://localhost:27017/test', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log('Connect success');
-    console.log(process.env.APP_URL);
-  })
-  .catch(err => {
-    console.log(err);
-  });
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
