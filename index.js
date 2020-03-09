2fcc241dbc8ad8b0897b1cdbc742fc268f5e62a9
Ngo Minh <nvminh023@gmail.com>
2020-03-09 10:34:30 +0700

    Config ci/cd

--- index.js
+++ index.js
@@ -3,6 +3,30 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.status(200).json({ msg: 'Hello World!' }));
+function run() {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res();
+    }, 4000);
+  });
+}
+
+app.get('/post1', async (req, res) => {
+  console.log('Starting post1');
+  // const start = Date.now();
+  // while (Date.now() < start + 3000) {}
+  await run();
+  console.log('End post1');
+  return res.send('Hello');
+});
+
+app.get('/post2', async (req, res) => {
+  console.log('Starting post2');
+  // const start = Date.now();
+  // while (Date.now() < start + 3000) {}
+  await run();
+  console.log('End post2');
+  return res.send('Hello post2');
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- index.test.js
+++ index.test.js
@@ -1,3 +1,23 @@
 test('Should return plus', () => {
   expect(2 + 2).toBe(4);
 });
+
+test('returns undefined by default', () => {
+  const mock = jest.fn();
+
+  let result = mock('foo');
+
+  expect(result).toBeUndefined();
+  expect(mock).toHaveBeenCalled();
+  expect(mock).toHaveBeenCalledTimes(1);
+  expect(mock).toHaveBeenCalledWith('foo');
+});
+
+test('mock return defined value', () => {
+  const fooFnc = value => value;
+  const mock = jest.fn(fooFnc);
+
+  mock.mockReturnValue('bar');
+
+  expect(mock('foo')).toBe('bar');
+});
